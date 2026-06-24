ea69c0702d238122885498a3e5db25078f70a198
ablejose <ablejose1029@gmail.com>
2026-06-24 23:36:40 +0530

    Lower confidence threshold

--- replace_plate.py
+++ replace_plate.py
@@ -1,16 +1,30 @@
 from ultralytics import YOLO
 import cv2
+import os
 
-# Load once when app starts
 model = YOLO("best.pt")
 logo = cv2.imread("logo.png")
 
 
 def process_image(input_path, output_path):
 
+    print("INPUT PATH:", input_path)
+    print("FILE EXISTS:", os.path.exists(input_path))
+
     image = cv2.imread(input_path)
 
-    results = model(image)
+    print("IMAGE IS NONE:", image is None)
+
+    if image is None:
+        raise Exception(f"Could not read image: {input_path}")
+
+    results = model(
+            image,
+            conf=0.15
+        )
+
+    for box in results[0].boxes:
+        print("CONF:", float(box.conf[0]))
 
     for box in results[0].boxes:
 
